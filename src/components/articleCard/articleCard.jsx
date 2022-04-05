e5cc333864d84e34a56a844c39d10fca561a95ab
Mateo <mtaycruz@gmail.com>
2022-04-05 09:35:35 -0400

    styling done on article page

--- src/components/articleCard/articleCard.jsx
+++ src/components/articleCard/articleCard.jsx
@@ -17,7 +17,7 @@ const ArticleCard = (props) => {
     return(
         
     <div className='m-5 rounded-lg cursor-pointer w-80 h-[32rem] hover:shadow-inner' onClick={routeChange}>
-        <img src={props.data.featured_media.medium} className="rounded-t-lg h-3/5 w-full"/>
+        <img src={props.data.featured_media.medium} className="rounded-t-lg h-3/5 w-full object-cover" alt={props.data.headline}/>
         <div className='bg-white text-center rounded-b-lg h-2/5'>
             <h1 className='text-[#8b8f98] text-xl font-bold pt-6'>{props.data.title}</h1>
             <h2 className='text-[#acb3be] text-sm font-light pt-6 px-1'>{props.data.excerpt}</h2>
